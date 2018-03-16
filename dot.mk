e2380667773a4fd8a7ce3c458cef4d5d4a27f3b7
Ganesh Varma <ganeshvarmai4u@gmail.com>
2018-03-17 00:58:57 +0530

    dotify

--- dot.mk
+++ dot.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := rr_mido
+PRODUCT_NAME := dot_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
